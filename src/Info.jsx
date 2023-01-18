2e31bae186b19944f39e9a2d7cb14e78235ab7f2
Ryan Hayame <100398101+ryanhayame@users.noreply.github.com>
2023-01-18 03:11:43 -0800

    Update Info.jsx

--- src/Info.jsx
+++ src/Info.jsx
@@ -7,7 +7,7 @@ export default function Info() {
     }
 
     const getResume = () => {
-        window.open("https://drive.google.com/file/d/1whUDEsKUhmNtpdIvtWePc8oq1q0Trmqt/view?usp=sharing", "_blank")
+        window.open("https://drive.google.com/file/d/1Ndh3Y_6mpxfPFGp1wKZnHzFpSBI1LAEv/view?usp=sharing", "_blank")
     }
 
     return (
